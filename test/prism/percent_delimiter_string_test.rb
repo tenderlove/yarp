1bee05792307780b3351b7a149984ca456b0e3d7
eileencodes <eileencodes@gmail.com>
2024-12-05 13:56:03 -0500

    Fix percent delimiter strings with crlfs
    parse.y treats CRLF as a LF and basically "normalizes" them before
    parsing.  That means a string like `%\nfoo\r\n` is actually treated as
    `%\nfoo\n` for the purposes of parsing.  This happens on both the
    opening side of the percent string as well as on the closing side.  So
    for example `%\r\nfoo\n` must be treated as `%\nfoo\n`.
    
    To handle this in Prism, when we start a % string, we check if it starts
    with `\r\n`, and then consider the terminator to actually be `\n`.  Then
    we check if there are `\r\n` as we lex the string and treat those as
    `\n`, but only in the case the start was a `\n`.
    
    Fixes: #3230
    
    [Bug #20938]
    
    Co-authored-by: John Hawthorn <jhawthorn@github.com>
    Co-authored-by: eileencodes <eileencodes@gmail.com>

---
+++ test/prism/percent_delimiter_string_test.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+module Prism
+  class PercentDelimiterStringTest < TestCase
+    def test_newline_terminator_with_crlf
+      str = "%\n123456\r\n"
+      assert_parse "123456", str
+    end
+
+    def test_newline_terminator_with_crcrlf
+      str = "%\n123456\r\r\n"
+      assert_parse "123456\r", str
+    end
+
+    def test_newline_terminator_with_crlf_pair
+      str = "%\r\n123456\r\n"
+      assert_parse "123456", str
+    end
+
+    def test_newline_terminator_with_crlf_opening
+      str = "%\r\n123456\r\r\n"
+      assert_parse "123456\r", str
+    end
+
+    def test_newline_terminator_with_cr_cr
+      str = "%\r123456\r;\n"
+      assert_parse "123456", str
+    end
+
+    def test_newline_terminator_with_crlf_lf
+      str = "%\r\n123456\n;\n"
+      assert_parse "123456", str
+    end
+
+    def test_cr_cr
+      str = "%\r1\r\n \r"
+      assert_parse "1", str
+    end
+
+    def test_lf_lf
+      str = "%\n1\r\n \n"
+      assert_parse "1", str
+    end
+
+    def assert_parse(expected, str)
+      tree = Prism.parse str
+      node = tree.value.breadth_first_search { |x| Prism::StringNode === x }
+      assert_equal expected, node.unescaped
+    end
+  end
+end
